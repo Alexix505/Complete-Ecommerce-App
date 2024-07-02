ded332a907564fa9e588680843bd17dd270262a6
Alexander Ikenna Orisa <115145901+Alexix505@users.noreply.github.com>
2024-07-02 18:33:28 +0100

    navbar done

--- lib/views/buyers/main_screen.dart
+++ lib/views/buyers/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,12 +9,51 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Main Screen'),
-      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _pageIndex,
+          onTap: (value) {
+            setState(() {
+              _pageIndex = value;
+            });
+          },
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.yellow.shade900,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                ),
+                label: 'HOME'),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/explore.svg',
+                ),
+                label: 'EXPLORE'),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/shop.svg',
+                ),
+                label: 'SHOP'),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/cart.svg',
+                ),
+                label: 'CART'),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/search.svg',
+                ),
+                label: 'SEARCH'),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/account.svg',
+                ),
+                label: 'ACCOUNT'),
+          ]),
     );
   }
 }
